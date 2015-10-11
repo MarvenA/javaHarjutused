67c173fce189768de0adf79b3cb13bbe43272c55
Marven Aus <maus@klass2-18-l.intra.itcollege.ee>
2015-10-11 14:28:09 +0300

    2. harjutustund, ristmiku lahendus

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,57 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+import lib.Foor.ValgusV2rv;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
+public class Peamurdja2_ristmik extends Application {
+
+    public void start (Stage primaryStage) throws Exception {
+        Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
+        Foor foor2 = new Foor(Foor.ALUMINE, primaryStage);
+        Foor foor3 = new Foor(Foor.PAREM, primaryStage);
+        Foor foor4 = new Foor(Foor.VASAK, primaryStage);
+
+        tsykkel(foor1, 0);
+        tsykkel(foor2, 0);
+        tsykkel(foor3, 1);
+        tsykkel(foor4, 1);
+    }
+
+    public void tsykkel (Foor foor, int suund) { // selle tsükliga käsutatakse nelja foori
+        int count = suund;
+        while (count <10) {
+        if (count != 0){
+            foor.vahetaPunast();
+            foor.paus(4.4);
+            foor.vahetaKollast();
+            foor.paus(0.6);
+            foor.vahetaKollast();
+            foor.vahetaPunast();
+        }
+            foor.vahetaRohelist();
+            foor.paus(3);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+
+            count = count +1;
+        }
+
+
+
+
+
+    }
 }
