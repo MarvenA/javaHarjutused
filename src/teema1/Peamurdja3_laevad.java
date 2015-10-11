dd1e114d73f3a29548b909e1007c96da5b946c1f
Marven Aus <maus@klass2-18-l.intra.itcollege.ee>
2015-10-11 16:38:45 +0300

    2 harjutustund

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -1,6 +1,9 @@
 package teema1;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,5 +18,53 @@ import java.util.Scanner;
  * 3. Kas oskad ka nii, et laevad ei ole 1x1 vaid 1x2 ja 1x3 ja orienteeruvad
  *    nii vertikaalselt kui ka horisontaalselt?
  */
+
+
 public class Peamurdja3_laevad {
+    public static void main(String[] args) {
+        int[][] openWater = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                openWater[i][j] = ship();
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            System.out.println(Arrays.toString(openWater[i]));
+        }
+
+        System.out.println(Arrays.toString(openWater));
+        Scanner user = new Scanner(System.in);
+
+        /*while (gameover(openWater)) {
+            System.out.println("Sisesta üks number: ");
+            int userInput = user.nextInt();
+            int hit = openWater[userInput];
+            if (hit == 0) {
+                System.out.print("Möödas...");
+            } else if (hit == 1) {
+                System.out.println("Pihtas!");
+                openWater[userInput] = 8;
+                System.out.println(Arrays.toString(openWater));
+            } else if (hit == 8) {
+                System.out.println("Oled siia juba proovinud!");
+            }
+        }
+        System.out.println("GAME OVER!! Kõik laevad põhjas!");
+    }*/
+
+    /*public static boolean gameover(int[] openWater) {
+        for (int i = 0; i < openWater.length; i++) {
+            if (openWater[i] == 1) {
+                return true;
+            }
+        }
+        return false;
+    }*/
+
+    private static int ship(){
+        return (int) (Math.random()*2);
+    }
 }
+
